d2fe5e0217fd2e47eb68079d5d498965e8d92818
usabarashi <usabarashi@mac.com>
2020-07-04 08:50:39 +0000

    add Collection

--- domain/type.py
+++ domain/type.py
@@ -1,7 +1,9 @@
 from dataclasses import dataclass
-from typing import Generic, Literal, TypeVar, Union
+from functools import reduce
+from typing import Callable, Generic, Literal, TypeVar, Union
 
 T = TypeVar('T')
+A = TypeVar('A')
 E = TypeVar('E')
 
 
@@ -22,3 +24,18 @@ class Err(Generic[E]):
 
 
 Result = Union[Err[E], Ok[T]]
+
+
+class Collection(list):
+
+    def __init__(self, iterable):
+        list.__init__(self, iterable)
+
+    def sort(self, *, key=None, reverse=False):
+        return Collection(sorted(self, key=key, reverse=reverse))
+
+    def map(self, *, function: Callable[[T], A]):
+        return Collection([function(element) for element in self])
+
+    def reduce(self, *, function: Callable[[T, T], T]) -> T:
+        return reduce(function, self)
--- domain/type_test.py
+++ domain/type_test.py
@@ -1,4 +1,5 @@
-from domain.type import Err, Literal, Ok, Result
+from domain.type import Collection, Err, Ok, Result
+from typing import Literal
 
 
 def test_result_ok():
@@ -17,3 +18,46 @@ def test_result_err():
     assert type(result(False)) is Err
     assert isinstance(result(False), Err)
     assert result(False).value == 'Err'
+
+
+def test_collection():
+    collection = Collection([0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
+    assert type(collection) == Collection
+    assert [0, 1, 2, 3, 4, 5, 6, 7, 8, 9] == collection
+
+
+def test_collection_sort():
+    collection = Collection([0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
+    sorted_collection = collection.sort()
+    assert id(sorted_collection) != id(collection)
+    assert [0, 1, 2, 3, 4, 5, 6, 7, 8, 9] == sorted_collection
+    sorted_collection = collection.sort(key=None)
+    assert id(sorted_collection) != id(collection)
+    assert [0, 1, 2, 3, 4, 5, 6, 7, 8, 9] == sorted_collection
+    sorted_collection = collection.sort(key=lambda x: x)
+    assert id(sorted_collection) != id(collection)
+    assert [0, 1, 2, 3, 4, 5, 6, 7, 8, 9] == sorted_collection
+    sorted_collection = collection.sort(reverse=False)
+    assert id(sorted_collection) != id(collection)
+    assert [0, 1, 2, 3, 4, 5, 6, 7, 8, 9] == sorted_collection
+    sorted_collection = collection.sort(reverse=True)
+    assert id(sorted_collection) != id(collection)
+    assert [9, 8, 7, 6, 5, 4, 3, 2, 1, 0] == sorted_collection
+    sorted_collection = collection.sort(key=lambda x: x, reverse=True)
+    assert id(sorted_collection) != id(collection)
+    assert [9, 8, 7, 6, 5, 4, 3, 2, 1, 0] == sorted_collection
+
+
+def test_collection_map():
+    collection = Collection([0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
+    mapped_collection = collection.map(function=lambda x: x * 2)
+    assert id(mapped_collection) != id(collection)
+    assert [0, 2, 4, 6, 8, 10, 12, 14, 16, 18] == mapped_collection
+
+
+def test_collection_recude():
+    collection = Collection([0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
+    reduced_collection = collection.reduce(
+        function=lambda left, right: left * right)
+    assert id(reduced_collection) != id(collection)
+    assert 0 == reduced_collection
